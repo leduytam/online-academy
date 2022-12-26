ac9ee85146e91522f032d02e7914166aa88d8d6b
Lê Duy Tâm <leduytam600@gmail.com>
2022-12-26 16:31:03 +0700

    OA-43  | Update categories dropdown menu

--- src/services/category.service.js
+++ src/services/category.service.js
@@ -2,7 +2,7 @@ import Category from '../models/category.model.js';
 import '../models/subcategory.model.js';
 
 const getAll = async () => {
-  const categories = await Category.find({}).populate('subcategories');
+  const categories = await Category.find({}).populate('subcategories').lean();
 
   return categories;
 };
