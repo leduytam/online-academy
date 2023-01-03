9be00d05af121820eb19fe0fc65214ef9354ab27
Tan Sang <btsang20@clc.fitus.edu.vn>
2023-01-03 12:20:58 +0700

    OA-22 | update information, file input and submit form

--- src/controllers/instructor.controller.js
+++ src/controllers/instructor.controller.js
@@ -5,8 +5,10 @@ const get = async (req, res, next) => {
 };
 
 const getInfo = async (req, res, next) => {
+  const { user } = req.session;
   res.render('instructor/profile', {
     title: 'Profile',
+    user,
   });
 };
 
