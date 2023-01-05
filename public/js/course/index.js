82f3bb8dc232e333bda7d74546a41177435b3116
Bun <hoangquocnam021117@gmail.com>
2023-01-06 05:07:14 +0700

    OA-20 | Implement the section + lesson list

--- public/js/course/index.js
+++ public/js/course/index.js
@@ -69,23 +69,99 @@ const createCourseInstructor = (instructor) => {
 
 const createUpdateTime = (updatedAt) => {
   const courseUpdateTimeElement = document.getElementById('course-header-updated-at');
+  const courseUpdateTimeIconElement = document.getElementById('course-header-updated-at-icon');
+
   courseUpdateTimeElement.innerHTML = `
     Last updated ${new Date(updatedAt).toLocaleDateString()}
   `
+  courseUpdateTimeIconElement.innerHTML = `
+    <i class="bi bi-clock-history"></i>
+  `
 }
 
+
+const createSectionLesson = (sections) => {
+  const titleElement = document.getElementById('course-section-title');
+  const descriptionElement = document.getElementById('course-section-description');
+
+  titleElement.innerHTML = `Course content`;
+  descriptionElement.innerHTML = `${sections.length} sections &#x2022; ${sections.reduce((a, b) => a + b.lessons.length, 0)} lessons`;
+
+  const sectionsAccordionElement = document.getElementById('course-lessons-accordion');
+  sections.forEach((section, index) => {
+    const accordionItemElement = document.createElement('div');
+    accordionItemElement.classList.add('accordion-item');
+
+    const accordionHeaderElement = document.createElement('h2');
+    accordionHeaderElement.classList.add('accordion-header');
+    accordionHeaderElement.id = `course-lessons-accordion-header-${index}`;
+
+    const accordionButtonElement = document.createElement('button');
+    accordionButtonElement.classList.add('accordion-button');
+    accordionButtonElement.type = 'button';
+    accordionButtonElement.setAttribute('data-bs-toggle', 'collapse');
+    accordionButtonElement.setAttribute('data-bs-target', `#course-lessons-accordion-item-collapse-${index}`);
+    accordionButtonElement.setAttribute('aria-expanded', 'true');
+    accordionButtonElement.setAttribute('aria-controls', `course-lessons-accordion-item-collapse-${index}`);
+
+    accordionButtonElement.innerHTML = `
+      <div class="d-flex justify-content-between w-100">
+        <div class="fw-bold">${section.name}</div>
+        <div class="fw-bold me-2">${section.lessons.length} lessons</div>
+      </div>
+    `
+
+    accordionHeaderElement.appendChild(accordionButtonElement);
+    accordionItemElement.appendChild(accordionHeaderElement);
+
+    const accordionCollapseElement = document.createElement('div');
+    accordionCollapseElement.id = `course-lessons-accordion-item-collapse-${index}`;
+    accordionCollapseElement.classList.add('accordion-collapse');
+    accordionCollapseElement.classList.add('collapse');
+    accordionCollapseElement.setAttribute('aria-labelledby', `course-lessons-accordion-header-${index}`);
+
+    const accordionBodyElement = document.createElement('div');
+    accordionBodyElement.classList.add('accordion-body');
+    accordionBodyElement.innerHTML = section.lessons.map((lesson, index) => {
+      // left is name of lesson and check the preview to show the preview text on the right
+      return `
+        <div class="d-flex align-items-center my-3">
+          <div class="me-2">
+            <i class="bi bi-play-circle-fill ms-1"></i>
+          </div>
+          <div class="d-flex justify-content-between w-100">
+            <a href="#" class="text-decoration-none text-dark">${lesson.name}</a>
+            ${lesson.preview ? `<a href="#" class="text-muted">Preview</a>` : ''}
+          </div>
+        </div>
+      `
+    }).join('');
+
+    accordionCollapseElement.appendChild(accordionBodyElement);
+    accordionItemElement.appendChild(accordionCollapseElement);
+  
+    sectionsAccordionElement.appendChild(accordionItemElement);
+  })
+
+}
 
 $(document).ready(function () {
   const courseId = window.location.pathname.split('/')[2];
   axios.get('/api/v1/courses/' + courseId).then((response) => {
     const { data } = response;
-    const { category, subcategory, name, briefDescription, reviews, enrollments, instructor, updatedAt } = data;
-    // console.log(data)
+    const { category, subcategory, name, briefDescription, reviews, enrollments, instructor, updatedAt, sections } = data;
+
+    console.log(data);
+
+    // header
     createBreadcrumb(category, subcategory, name);
     createTitle(name);
     createBriefDescription(briefDescription);
     createReviews(reviews, enrollments);
     createCourseInstructor(instructor);
     createUpdateTime(updatedAt);
+
+    // body
+    createSectionLesson(sections);
   });
 });
--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,7 +1,9 @@
 import Category from '../models/category.model.js';
 import Course from '../models/course.model.js';
 import Enrollment from '../models/enrollment.model.js';
+import Lesson from '../models/lesson.model.js';
 import Review from '../models/review.model.js';
+import Section from '../models/section.model.js';
 import Subcategory from '../models/subcategory.model.js';
 import User from '../models/user.model.js';
 
@@ -24,6 +26,22 @@ const getCourseDetailApi = async (req, res, next) => {
     const reviews = await Review.find({ course: course._id });
     const enrollments = await Enrollment.find({ course: course._id });
     const instructor = await User.findById(course.instructor);
+    // query all lessons, sections by course's sessions
+    const sections = await Promise.all(
+      course.sections.map(async (sectionId) => {
+        const section = await Section.findById(sectionId);
+        const lessons = await Promise.all(
+          section.lessons.map(async (sectionLesson) => {
+            const lesson = await Lesson.findById(sectionLesson);
+            return lesson;
+          })
+        );
+        return {
+          ...section.toObject(),
+          lessons,
+        };
+      })
+    );
 
     const result = {
       ...course.toObject(),
@@ -32,6 +50,7 @@ const getCourseDetailApi = async (req, res, next) => {
       reviews,
       enrollments,
       instructor,
+      sections,
     };
     res.status(200).send(result);
   } catch (error) {
