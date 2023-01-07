1ecee39fe4d69905929dffaf670ba77e0753fe0d
Tan Sang <btsang20@clc.fitus.edu.vn>
2023-01-07 18:33:16 +0700

    OA-57 | update logic for id, delete fetch image

--- src/controllers/instructor.controller.js
+++ src/controllers/instructor.controller.js
@@ -13,7 +13,7 @@ const get = async (req, res, next) => {
 
 const getInfo = async (req, res, next) => {
   const { user } = req.session;
-  const userDisplay = await User.findById(user._id).lean();
+  const userDisplay = await User.findById(user._id ?? '').lean();
   res.render('instructor/profile', {
     title: 'Profile',
     user: userDisplay,
@@ -23,7 +23,7 @@ const getInfo = async (req, res, next) => {
 const updateInformation = async (req, res, next) => {
   try {
     const { user } = req.session;
-    const userUpdate = await User.findById(user._id);
+    const userUpdate = await User.findById(user._id ?? '');
     const { name, facebook, website, twitter, linkedin, bio, youtube } =
       req.body;
     userUpdate.name = name;
@@ -47,7 +47,7 @@ const updateInformation = async (req, res, next) => {
 const uploadImage = async (req, res, next) => {
   const { file } = req;
   const { user } = req.session;
-  const userUpdate = await User.findById(user._id);
+  const userUpdate = await User.findById(user._id ?? '');
 
   if (!file) {
     req.session.error = 'Please upload an image';
@@ -66,7 +66,6 @@ const uploadImage = async (req, res, next) => {
     await userUpdate.save();
     req.session.user = userUpdate;
     await gcsService.uploadImage(file, slug);
-    req.session.success = await gcsService.getPublicImageUrl(slug);
   } catch (error) {
     req.session.error = error.message;
   }
