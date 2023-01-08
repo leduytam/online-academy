16f8f6568df978541b25bab44f83310f55574e85
hqnam <81608889+hoangquocnam@users.noreply.github.com>
2023-01-08 16:43:50 +0700

    OA-58 | Implement API for course (#41)
    * OA-58 | api - created course
    
    * OA-58 | CRUD for course
    
    * OA-58 | CRUD lesson + section

--- src/routes/instructor.route.js
+++ src/routes/instructor.route.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import multer from 'multer';
 
+import ERole from '../constant/role.js';
 import InstructorController from '../controllers/instructor.controller.js';
+import auth from '../middlewares/auth.middleware.js';
 import gcsService from '../services/gcs.service.js';
 
 const upload = multer({
@@ -19,8 +21,48 @@ router.get('/profile', InstructorController.getInfo);
 
 router.route('/edit-profile').post(InstructorController.updateInformation);
 
-router.post('/upload-image', upload.single('image'), async (req, res, next) => {
-  InstructorController.uploadImage(req, res, next, gcsService);
-});
+router.post(
+  '/upload-profile-image',
+  upload.single('image'),
+  async (req, res, next) => {
+    InstructorController.uploadProfileImage(req, res, next, gcsService);
+  }
+);
+//  course
+router.get(
+  '/courses',
+  auth.protect,
+  auth.restrictTo(ERole.INSTRUCTOR),
+  InstructorController.getCoursesView
+);
+router.get(
+  '/courses/:courseSlug',
+  auth.protect,
+  auth.restrictTo(ERole.INSTRUCTOR),
+  InstructorController.getCourseView
+);
+
+// section
+router.get(
+  'courses/:courseSlug/sections',
+  auth.protect,
+  auth.restrictTo(ERole.INSTRUCTOR),
+  InstructorController.getSectionsView
+);
+
+router.get(
+  '/courses/:courseSlug/:sectionId',
+  auth.protect,
+  auth.restrictTo(ERole.INSTRUCTOR),
+  InstructorController.getSectionView
+);
+
+// lesson
+router.get(
+  '/courses/:courseSlug/:sectionId/:lessonSlug',
+  auth.protect,
+  auth.restrictTo(ERole.INSTRUCTOR),
+  InstructorController.getLessonView
+);
 
 export default router;
