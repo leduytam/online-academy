8f0c1f3c69e305dbbeb073c6f6b439ef00fe24cd
Lê Duy Tâm <leduytam600@gmail.com>
2023-01-08 19:43:40 +0700

    OA-63 | Replace gcs key file with base64 encoded gcs key env

--- src/configs/index.js
+++ src/configs/index.js
@@ -55,13 +55,12 @@ const configs = {
     maxAge: ms(env.OTP_MAX_AGE),
   },
   gcs: {
-    storage: {
-      projectId: env.GCS_PROJECT_ID,
-      keyFilename: env.GCS_KEY_FILE,
-    },
     publicBucket: env.GCS_PUBLIC_BUCKET,
     privateBucket: env.GCS_PRIVATE_BUCKET,
     signedUrlExpiresIn: ms(env.GCS_SIGNED_URL_EXPIRES_IN),
+    credentials: JSON.parse(
+      Buffer.from(env.GCS_CREDENTIALS_ENCODED, 'base64').toString('utf8')
+    ),
   },
   apiUrl: env.API_URL,
 };
